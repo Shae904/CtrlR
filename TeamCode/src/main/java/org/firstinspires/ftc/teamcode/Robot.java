a19c56ea68d6c5064777158176857bc6a38838f8
shaizebos <shailen0904@gmail.com>
2025-10-15 15:10:47 -0400

    adding robot + teleop files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class Robot {
+
+    public final IMU imu;
+    public final DcMotor fl, fr, bl, br;
+
+    private final LinearOpMode opMode;
+    public Robot(LinearOpMode opMode) {
+        this.opMode = opMode;
+        HardwareMap hardwareMap = opMode.hardwareMap;
+
+        // Drivetrain
+        fl = hardwareMap.dcMotor.get("fl");
+        fr = hardwareMap.dcMotor.get("fr");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+
+        fl.setMode(RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(RunMode.RUN_WITHOUT_ENCODER);
+
+        fl.setDirection(Direction.REVERSE);
+        fr.setDirection(Direction.FORWARD);
+        bl.setDirection(Direction.REVERSE);
+        br.setDirection(Direction.FORWARD);
+
+        fl.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+        imu.resetYaw();
+
+    }
+
+    public double getHeading() {
+        return this.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+    }
+
+    public void setDriveTrainPower(double frPow, double flPow, double brPow, double blPow) {
+        fr.setPower(frPow);
+        fl.setPower(flPow);
+        br.setPower(brPow);
+        bl.setPower(blPow);
+    }
+    public void shoot(){
+
+    }
+    public void intake(){
+
+    }
+    public void cycle(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp(name = "Teleop")
+public class Teleop extends LinearOpMode{
+
+    Robot robot;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(this);
+        waitForStart();
+        // START
+
+        // LOOP
+        while (opModeIsActive()) {
+
+            if (gamepad1.left_bumper) {
+                robot.imu.resetYaw();
+            }
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            double botHeading = AngleUnit.DEGREES.toRadians(robot.getHeading());
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            // Slow mode
+            if (gamepad1.right_bumper) {
+                frontLeftPower *= 0.4;
+                backLeftPower *= 0.4;
+                frontRightPower *= 0.4;
+                backRightPower *= 0.4;
+            }
+
+            robot.fl.setPower(frontLeftPower);
+            robot.bl.setPower(backLeftPower);
+            robot.fr.setPower(frontRightPower);
+            robot.br.setPower(backRightPower);
+
+            if(gamepad1.a){
+                robot.shoot();
+                telemetry.addData("shoot", "shoot");
+            }
+            else if(gamepad1.x){
+                robot.intake();
+                telemetry.addData("intake", "intake");
+            }
+            else if(gamepad1.y){
+                robot.cycle();
+                telemetry.addData("cycle", "cycle");
+            }
+        }
+    }
+}
